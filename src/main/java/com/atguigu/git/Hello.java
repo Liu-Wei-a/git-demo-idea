667b2c611ef86e5b00bee018aea203b800c44d16
lw <397209978@qq.com>
2022-08-31 20:00:36 +0800

    第2次提交

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world~~");
+        System.out.println("2222222222222");
     }
 }
