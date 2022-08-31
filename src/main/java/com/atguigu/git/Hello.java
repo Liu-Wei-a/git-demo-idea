9901e318629c5ad0fec5aad1be218b7617ba0020
lw <397209978@qq.com>
2022-08-31 20:03:10 +0800

    hello3

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world~~");
         System.out.println("2222222222222");
+        System.out.println("333333333333");
     }
 }
